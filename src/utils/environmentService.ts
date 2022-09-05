daa2c4dc8a49c67fb0858fdd74bf6aac17e7ab3d
Paul DeVito <pdevito3@gmail.com>
2022-09-05 00:19:41 -0400

    minor env cleanup

--- src/utils/environmentService.ts
+++ src/utils/environmentService.ts
@@ -4,7 +4,7 @@ export const env = {
   isDevelopment: _env === "development",
   auth: {
     nextAuthId: "oidc",
-    secret: "974d6f71-d41b-4601-9a7a-a33081f82188", // TODO change to env var ref
+    secret: process.env.NEXTAUTH_SECRET,
   },
   clientUrls: {
     recipeManagement: () => {
