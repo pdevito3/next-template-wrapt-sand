3393dcb01866a247bb223b8df3088ed6142acc90
Paul DeVito <pdevito3@gmail.com>
2022-09-12 22:39:15 -0400

    fix: overflow on main

--- src/components/PrivateLayout.tsx
+++ src/components/PrivateLayout.tsx
@@ -9,9 +9,9 @@ export default function PrivateLayout({ children }: Props) {
   return (
     <div className="flex flex-col w-full h-full">
       <PrivateHeader />
-      <div className="flex h-full overflow-auto bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-white">
+      <div className="flex h-full bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-white">
         <PrivateSideNav />
-        <main className="flex-1 px-4 py-2 bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-white sm:px-6 md:py-4 md:px-8">
+        <main className="flex-1 px-4 py-2 overflow-auto bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-white sm:px-6 md:py-4 md:px-8">
           {children}
         </main>
       </div>
