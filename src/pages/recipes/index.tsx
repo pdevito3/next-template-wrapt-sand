8e1608026cf1b94d640861ebc1044dc40053dad4
Paul DeVito <pdevito3@gmail.com>
2022-09-15 22:20:39 -0400

    update: search input abstraction

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -1,9 +1,9 @@
 import { PaginatedTableProvider, useGlobalFilter } from "@/components/Forms";
-import DebouncedInput from "@/components/Forms/DebouncedInput";
+import SearchInput from "@/components/SearchInput";
 import { RecipeListTable } from "@/domain/recipes";
 import "@tanstack/react-table";
 import Link from "next/link";
-import { IconCirclePlus, IconSearch } from "tabler-icons";
+import { IconCirclePlus } from "tabler-icons";
 import PrivateLayout from "../../components/PrivateLayout";
 
 RecipeList.isPublic = false;
@@ -25,18 +25,12 @@ export default function RecipeList() {
             {/* prefer this. more composed approach */}
             <PaginatedTableProvider>
               <div className="flex items-center justify-between">
-                {/* TODO: abstract to an input that can use the debounce input under the hood */}
-                <div className="relative mt-1">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <IconSearch className="w-5 h-5 text-gray-500 dark:text-gray-400" />
-                  </div>
-
-                  <DebouncedInput
+                <div className="mt-1">
+                  <SearchInput
                     value={globalRecipeFilter ?? ""}
                     onChange={(value) =>
                       calculateAndSetQueryFilterForRecipes(String(value))
                     }
-                    className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg outline-none w-80 bg-gray-50 focus:border-violet-500 focus:ring-violet-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-violet-500 dark:focus:ring-violet-500"
                     placeholder="Search all columns..."
                   />
                 </div>
